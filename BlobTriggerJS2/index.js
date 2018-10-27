fc4ce5ff4a617432c35abe0a22eaedba27fcb54a
Mike Shaunnessey <mshaunnessey@Mike-Shaunnesseys-MacBook-Pro.local>
2018-10-27 16:51:49 -0400

    a

--- BlobTriggerJS2/index.js
+++ BlobTriggerJS2/index.js
@@ -1,7 +1,6 @@
 module.exports = function (context, myBlob) {
     context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name );
     context.log("Input: ",myBlob)
-//    var jobj = JSON.parse(myBlob);
-    context.log("Parsed faces: ",myBlob.faces[0].gender);
+    context.log("Parsed faces: ",myBlob.faces.length);
     context.done();
 };
