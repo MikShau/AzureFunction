6028bd5a7e057ad76fc6fb003057a4f128ce2514
Mike Shaunnessey <mshaunnessey@Mike-Shaunnesseys-MacBook-Pro.local>
2018-10-27 16:38:48 -0400

    json test

--- BlobTriggerJS2/index.js
+++ BlobTriggerJS2/index.js
@@ -2,6 +2,6 @@ module.exports = function (context, myBlob) {
     context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name );
     context.log("Input: ",myBlob)
     var jobj = JSON.parse(myblob);
-    context.log("Parsed faces: ",jobj.faces[0].gender);
+//    context.log("Parsed faces: ",jobj.faces[0].gender);
     context.done();
 };
