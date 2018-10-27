1a06a2bfaee48d093b263cd8e78475beaf596484
Mike Shaunnessey <mshaunnessey@Mike-Shaunnesseys-MacBook-Pro.local>
2018-10-27 15:44:51 -0400

    added jasonparse

--- BlobTriggerJS2/index.js
+++ BlobTriggerJS2/index.js
@@ -1,5 +1,6 @@
 module.exports = function (context, myBlob) {
     context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name );
     context.log("Input: ",myBlob)
+    var jobj = JSON.parse(myblob);
     context.done();
 };
