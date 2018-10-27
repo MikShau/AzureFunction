d79b9e3719d019adf84ab9e33aaa4bcfd02666f6
Mike Shaunnessey <mshaunnessey@Mike-Shaunnesseys-MacBook-Pro.local>
2018-10-27 15:02:07 -0400

    json testing

--- BlobTriggerJS2/index.js
+++ BlobTriggerJS2/index.js
@@ -1,5 +1,5 @@
 module.exports = function (context, myBlob) {
     context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name );
-    context.log("Input: ",myBlob.faces)
+    context.log("Input: ",myBlob)
     context.done();
 };
