be5032e9e60209d1347ec67a2f6a176a87a99dc7
Mike Shaunnessey <mshaunnessey@Mike-Shaunnesseys-MacBook-Pro.local>
2018-11-03 11:10:56 -0400

    a

--- BlobTriggerJS1/index.js
+++ BlobTriggerJS1/index.js
@@ -37,7 +37,8 @@ module.exports = function (context, myBlob) {
     //
     request(options, function(err, res, body) {
         let json = JSON.parse(body);
-        context.log("Blob Uri: ",context.bindingData.name) ;
+        context.log("Blob Uri: ",context.bindingData.blobTrigger) ;
+        json.blobpath = context.bindingData.blobTrigger ;
             blobService.createBlockBlobFromText(containerName,context.bindingData.name +".json", body, function(error, result, response) {
                 if (!error) {
                 // file uploaded
