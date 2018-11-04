4821947f416027d4480d450d70de255492c3e44a
Mike Shaunnessey <mshaunnessey@Mike-Shaunnesseys-MacBook-Pro.local>
2018-11-04 08:43:33 -0500

    a

--- BlobTriggerJS2/index.js
+++ BlobTriggerJS2/index.js
@@ -1,5 +1,5 @@
 module.exports = function (context, myBlob) {
     context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name );
-    context.log("Found ",myBlob.faces.length, "Faces in "myBlob.bloburl);
+    context.log("Found ",myBlob.faces.length, "Faces in ",myBlob.bloburl);
     context.done();
 };
