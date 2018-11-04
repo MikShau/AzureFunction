6ac79ef3f3e01dc007b66af94987469fa6155616
Mike Shaunnessey <mshaunnessey@Mike-Shaunnesseys-MacBook-Pro.local>
2018-11-04 11:21:38 -0500

    a

--- BlobTriggerJS2/index.js
+++ BlobTriggerJS2/index.js
@@ -1,5 +1,16 @@
 module.exports = function (context, myBlob) {
     context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name );
     context.log("Found ",myBlob.faces.length, "Faces in ",myBlob.bloburl);
+    //
+    // Set up face call
+    // 
+    const subscriptionKey = process.env.CogServiceKey ;
+    const params = {
+    'visualFeatures' : 'categories,tags,description,faces',
+     'details': '',
+    'language': 'en'
+     };
+
+
     context.done();
 };
